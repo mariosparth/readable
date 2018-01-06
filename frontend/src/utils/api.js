a0029f97c015e3aa92f597a7862934414fa273e1
Marios Parthenios <marios.parthenios@gmail.com>
2018-01-06 20:18:56 +0200

    add get api calls

---
+++ frontend/src/utils/api.js
@@ -0,0 +1,41 @@
+const api = "http://localhost:3001/"
+
+// Generate token
+
+let token = localStorage.token
+if (!token)
+    token = localStorage.token = Math.random().toString(36).substr(-8)
+
+const headers = {
+    'Accept': 'Application/json',
+    'Authorization': token
+}
+
+
+export const getCategories = () =>
+    fetch(`${api}/categories`, {
+        headers
+    })
+    .then(res => res.json())
+    .then(data => data.categories)
+
+export const getCategoryPosts = (categoryName) =>
+    fetch(`${api}/${categoryName}/posts`, {
+        headers
+    })
+    .then(res => res.json)
+    .then(data => data.categoryPosts)
+
+export const getAllPosts = () =>
+    fetch(`${api}/posts`, {
+        headers
+    })
+    .then(res => res.json())
+    .then(data => data.allPosts)
+
+export const getPost = (postId) =>
+    fetch(`${api}/postId`, {
+        header
+    })
+    .then(res => res.json())
+    .then(data => data.post)
