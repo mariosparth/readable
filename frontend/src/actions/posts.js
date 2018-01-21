09e8c8b26a480e68fddc30444c07a04288399f43
Dimitra <marios.parthenios@gmail.com>
2018-01-21 12:57:21 +0200

    add post action

--- frontend/src/actions/posts.js
+++ frontend/src/actions/posts.js
@@ -1,26 +1,30 @@
-import {
-    GET_ALL_POSTS,
-    GET_POSTS_BY_CATEGORY
-} from './types';
+import { GET_ALL_POSTS, GET_POSTS_BY_CATEGORY, ADD_POST } from "./types";
 import * as api from "../utils/api";
 
-export const getAllPosts = (posts) => {
-    return {
-        type: GET_ALL_POSTS,
-        posts
-    };
-};
+export const getAllPosts = posts => ({
+  type: GET_ALL_POSTS,
+  posts,
+});
 
-export const getPostsByCategory = (posts, category) => {
-    return {
-        type: GET_POSTS_BY_CATEGORY,
-        posts,
-        category
-    }
+export const getPostsByCategory = (posts, category) => ({
+  type: GET_POSTS_BY_CATEGORY,
+  posts,
+  category,
+});
+
+export const fetchAllPosts = () => dispatch =>
+  api.getAllPosts().then(posts => dispatch(getAllPosts(posts)));
+
+export const addPostAction = (post) => {
+  return {
+    type: ADD_POST,
+    post,
+  }
 }
 
-export const fetchAllPosts = () => (dispatch) => {
-    return api.getAllPosts()
-        .then(posts => dispatch(getAllPosts(posts)));
+export const addPost = (postData) => (dispatch) => {
+  return api.addPost(postData)
+    .then(postData => dispatch(addPostAction(postData)))
 }
+
 
--- frontend/src/actions/types.js
+++ frontend/src/actions/types.js
@@ -1,3 +1,4 @@
 export const GET_CATEGORIES = 'GET_CATEGORIES';
 export const GET_ALL_POSTS = 'GET_ALL_POSTS';
 export const GET_POSTS_BY_CATEGORY = 'GET_POSTS_BY_CATEGORY';
+export const ADD_POST = 'ADD_POST';
